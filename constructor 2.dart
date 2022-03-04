0fccea03f5694ce604df5a5965c91db9810d1b75
Smohana-Priya <100066754+Smohana-Priya@users.noreply.github.com>
2022-03-04 17:35:41 +0530

    Add files via upload

---
+++ constructor 2.dart
@@ -0,0 +1,34 @@
+import 'dart:html';
+
+class BankAccount
+{
+  BankAccount(
+  {
+    required this.accountHolder,
+    this.balance=0,
+  });
+  String accountHolder;
+  int balance;
+  void deposit(int amount)
+  {
+    balance+=amount;
+  }
+  bool withdraw(int amount)
+  {
+    if(amount<balance)
+    {
+    balance-=amount;
+    return  true;
+    } 
+    else
+   {
+    return false;
+   }
+}
+    }
+    void main()
+    {
+final obj=BankAccount(accountHolder:'priya',balance:100);
+print(obj.balance);
+    }
+  

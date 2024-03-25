79f9a34391e2b5b167f58e30b7aeab974d51cfd7
Smohana-Priya <smohanapriya2604@gmail.co>
2024-03-25 16:22:31 +0530

    added new method

--- palindrome.dart
+++ palindrome.dart
@@ -1,14 +1,27 @@
-import 'dart:io';
-void main()
-{
-  stdout.write('enter the string');
-var input=stdin.readLineSync();
-  var rev=input!.split('').reversed.join('');
-  if(input == rev)
-{
-   print('its palindrome');
+void main() {
+  /// String
+  var input = 'madam';
+  var rev = input.split('').reversed.join('');
+  if (input == rev) {
+    print('The string is palindrome');
+  } else {
+    print('The string is not palindrome');
   }
-else{
-    print('its not palindrome');
+
+  /// Number
+  int number = 1214666;
+  int revNum = 0;
+  while (number != 0) {
+    int lastDigit = number % 10;
+
+    revNum = revNum * 10 + lastDigit;
+
+    number = number ~/ 10;
+  }
+  print('Reversed Number is ---> $revNum');
+  if (number == revNum) {
+    print('The number is Palindrome');
+  } else {
+    print('The number not palindrome');
   }
 }

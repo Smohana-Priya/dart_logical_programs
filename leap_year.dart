f254762f9df101ba6516892fec8881d022548eed
Smohana-Priya <smohanapriya2604@gmail.co>
2024-03-26 16:45:56 +0530

    leap year

---
+++ leap_year.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter a year to find lear year or not: ');
+  int year = int.parse(stdin.readLineSync()!);
+  if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    print('$year is a leap year');
+  }
+  print('$year is not a leap year');
+}

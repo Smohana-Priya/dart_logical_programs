950fe3e856a2bf4a05548759fa7fd72cdfd6a3ce
Smohana-Priya <smohanapriya2604@gmail.co>
2024-03-26 10:25:25 +0530

    prime number

--- primeNumber.dart
+++ primeNumber.dart
@@ -1,21 +1,23 @@
-import 'dart:io';
-void main()
-{
-stdout.write('enter the number');
-int num=int.parse(stdin.readLineSync()!);
-checkPrime(num);
-}
-void checkPrime(int num)
-{
-for(var i=1;i<=num;i++)
-{
-if(num%i==0)
-{
-  print('not prime');
-  break;
-}
-else{
-  print('prime');
-}
-}
+void main() {
+  int num = 4;
+
+  /// m = 5 ~/ 2 = 2
+  int m = num ~/ 2;
+  bool isPrime = true;
+
+  /// i = 2; i<= 2; i++
+  for (var i = 2; i <= m; i++) {
+    /// 5 % 2 == 0
+    /// so its not safiesfy
+    if (num % i == 0) {
+      isPrime = false; // If num is divisible by i, it's not prime
+      break;
+    }
+  }
+
+  if (isPrime) {
+    print('$num is a prime number.');
+  } else {
+    print('$num is not a prime number.');
+  }
 }

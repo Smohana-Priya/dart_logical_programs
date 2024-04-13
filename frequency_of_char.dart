beaa533d69945e0a38f741cd16d6a1aedd8f1014
Smohana-Priya <smohanapriya2604@gmail.co>
2024-04-13 13:30:08 +0530

    added words frequency

--- frequency_of_char.dart
+++ frequency_of_char.dart
@@ -7,4 +7,22 @@ void main() {
     charFrequency[str[i]] = (charFrequency[str[i]] ?? 0) + 1;
   }
   print(charFrequency);
+
+  /// word frequency
+  List<String> words = [
+    'banana',
+    'apple',
+    'banana',
+    'grapes',
+    'goya',
+    'goya',
+    'goya',
+    'apple'
+  ];
+  Map<String, int> freqMap = {};
+  for (String word in words) {
+    freqMap[word] = (freqMap[word] ?? 0) + 1;
+    // print(freqMap);
+  }
+  print(freqMap);
 }

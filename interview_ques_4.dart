af78e996877efcc777c61b98dbd3794c1537f47b
Smohana-Priya <smohanapriya2604@gmail.co>
2024-05-13 11:59:49 +0530

    added new question

---
+++ interview_ques_4.dart
@@ -0,0 +1,9 @@
+import 'dart:core';
+
+void main() {
+  String input = "interview2021@syncfusion.com";
+  String pattern = r'[^a-zA-Z]';
+  RegExp regex = RegExp(pattern);
+  String result = input.replaceAll(regex, '');
+  print(result);
+}

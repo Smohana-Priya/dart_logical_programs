2ee129ea27303986a473bd15d4b29970bbea6e08
Smohana-Priya <smohanapriya2604@gmail.co>
2024-05-20 13:26:48 +0530

    miner changes

--- factorial.dart
+++ factorial.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  /// 5 factorial means 5*4*3*2*1 = 125
+  /// 5 factorial means 5*4*3*2*1 = 120
   int res = 1;
   stdout.write('Enter a number to find factorial: ');
   int num = int.parse(stdin.readLineSync()!);
--- interview_ques_1.dart
+++ interview_ques_1.dart
@@ -1,12 +1,16 @@
 ///  I/p [1,2,3]
 /// o/p [6,3,2]
 void main() {
-  List<int> input = [1, 2, 3];
+  List<int> input = [5, 2, 3];
   List<int> output = [];
 
   /// to find the output --> multiply all elements from the list and divide this using current element
   /// here res i = 6
-  int res = input.reduce((value, element) => value * element);
+  int res = input.reduce((value, element) {
+    print('value is $value and element is $element');
+    return value * element;
+  });
+  print(res);
   for (int i = 0; i < input.length; i++) {
     /// here i divide total prod by current element
     int value = res ~/ input[i];

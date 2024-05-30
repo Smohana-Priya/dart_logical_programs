52c81ce8a42c370759cd1f04d3b062edb46c6ebf
Smohana-Priya <smohanapriya2604@gmail.co>
2024-05-30 09:31:54 +0530

    minor changes

--- leap_year.dart
+++ leap_year.dart
@@ -5,6 +5,7 @@ void main() {
   int year = int.parse(stdin.readLineSync()!);
   if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
     print('$year is a leap year');
+  } else {
+    print('$year is not a leap year');
   }
-  print('$year is not a leap year');
 }
